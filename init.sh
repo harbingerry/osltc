01ae9115ffd82fe77c444fdad54ee6003ecaef05
Chris <carbon@gmx.com>
2013-02-04 11:23:06 -0500

    - added start hook?

--- init.sh
+++ init.sh
@@ -5,10 +5,15 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-cd ~/diy-0.1/data
+DTA=~/diy-0.1/data
+
+cd $DTA
 git clone git://github.com/pooler/cpuminer.git
 cd cpuminer
 ./autogen.sh
 ./configure CFLAGS="-O3"
 make
+cat > $DTA/start <<EOF
 tmux new -d -s miner "./minerd --url http://mine.pool-x.eu --userpass xomelle.$1:x"
+EOF
+chmod +x $DTA/start
